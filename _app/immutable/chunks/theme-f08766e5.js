38bf41fdbf2867a7835864eb4498b5b9f7a5b686
Jeff Zhao <jeff.no.zhao@gmail.com>
2022-09-16 18:17:51 -0400

    update website

--- _app/immutable/chunks/theme-f08766e5.js
+++
@@ -1,1 +0,0 @@
-import{w as a}from"./index-9cc81fa5.js";const l="dark";var e;const o=(e=window.localStorage.getItem("theme"))!=null?e:l,r=a(o==="dark");r.subscribe(t=>{t?localStorage.setItem("theme","dark"):localStorage.setItem("theme","light")});export{r as d};
