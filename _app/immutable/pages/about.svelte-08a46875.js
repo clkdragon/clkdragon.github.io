f2bec87d90a589aaf1a6ec57c78a31b23dec81e4
Jeff Zhao <jeff.no.zhao@gmail.com>
2022-09-16 13:33:33 -0400

    update website

---
+++ _app/immutable/pages/about.svelte-08a46875.js
@@ -0,0 +1,59 @@
+import{S as O,i as M,s as H,l as u,a as x,w as R,M as P,m,h as n,c as h,n as j,x as S,p as U,H as V,b as r,y as A,f as E,t as C,B as z,r as y,u as v,E as G}from"../chunks/index-3ade47a4.js";import{N as J,F as Q,T as N}from"../chunks/Footer-b9c867db.js";import{S as X}from"../chunks/SidePadding-cc73c228.js";import"../chunks/theme-f08766e5.js";import"../chunks/index-9cc81fa5.js";function Y(_){let a;return{c(){a=y("About Us")},l(f){a=v(f,"About Us")},m(f,o){r(f,a,o)},d(f){f&&n(a)}}}function Z(_){let a,f,o,b,c,s,w,p,d,e,i,$,B,I,g,k,D,F,L,T,W,q,K;return{c(){a=u("br"),f=y(`
+			CLK Lion Dance and Kung Fu has been led by Sifu Sammy Cheng since 1990.
+			`),o=u("br"),b=u("br"),c=y(`
+			Since our inception, we've performed hundreds of weddings and have travelled across Canada and
+			the world to train, compete, and bring excitement to people's special events.
+			`),s=u("br"),w=u("br"),p=y(`
+			We specialize in Lion Dances for weddings, bringing good luck, fortune, and energy to crowds across
+			Toronto. We'll provide a traditional performance with a modern flare.
+			`),d=u("br"),e=u("br"),i=y(`
+			Feed the lion, and if you are up for it, we can even invite you and your partner into the lion
+			yourself mid-show, and you can show off your lion dance style, biting and and playing with your
+			loved one's on your special day.
+			`),$=u("br"),B=u("br"),I=y(`
+			Or we can keep it traditional.
+
+			`),g=u("br"),k=u("br"),D=y(`
+			Either way, our award winning team will customize for your unique occasion and bring a performance
+			to remember.
+
+			`),F=u("br"),L=u("br"),T=y(`
+			Visit us at our martial arts club in Scarborough and see what we are all about. We'd be honored
+			to be part of your special event and will work closely with you to create a performance to remember!
+
+			`),W=u("br"),q=u("br"),K=y(`
+			See you soon!`)},l(t){a=m(t,"BR",{}),f=v(t,`
+			CLK Lion Dance and Kung Fu has been led by Sifu Sammy Cheng since 1990.
+			`),o=m(t,"BR",{}),b=m(t,"BR",{}),c=v(t,`
+			Since our inception, we've performed hundreds of weddings and have travelled across Canada and
+			the world to train, compete, and bring excitement to people's special events.
+			`),s=m(t,"BR",{}),w=m(t,"BR",{}),p=v(t,`
+			We specialize in Lion Dances for weddings, bringing good luck, fortune, and energy to crowds across
+			Toronto. We'll provide a traditional performance with a modern flare.
+			`),d=m(t,"BR",{}),e=m(t,"BR",{}),i=v(t,`
+			Feed the lion, and if you are up for it, we can even invite you and your partner into the lion
+			yourself mid-show, and you can show off your lion dance style, biting and and playing with your
+			loved one's on your special day.
+			`),$=m(t,"BR",{}),B=m(t,"BR",{}),I=v(t,`
+			Or we can keep it traditional.
+
+			`),g=m(t,"BR",{}),k=m(t,"BR",{}),D=v(t,`
+			Either way, our award winning team will customize for your unique occasion and bring a performance
+			to remember.
+
+			`),F=m(t,"BR",{}),L=m(t,"BR",{}),T=v(t,`
+			Visit us at our martial arts club in Scarborough and see what we are all about. We'd be honored
+			to be part of your special event and will work closely with you to create a performance to remember!
+
+			`),W=m(t,"BR",{}),q=m(t,"BR",{}),K=v(t,`
+			See you soon!`)},m(t,l){r(t,a,l),r(t,f,l),r(t,o,l),r(t,b,l),r(t,c,l),r(t,s,l),r(t,w,l),r(t,p,l),r(t,d,l),r(t,e,l),r(t,i,l),r(t,$,l),r(t,B,l),r(t,I,l),r(t,g,l),r(t,k,l),r(t,D,l),r(t,F,l),r(t,L,l),r(t,T,l),r(t,W,l),r(t,q,l),r(t,K,l)},p:G,d(t){t&&n(a),t&&n(f),t&&n(o),t&&n(b),t&&n(c),t&&n(s),t&&n(w),t&&n(p),t&&n(d),t&&n(e),t&&n(i),t&&n($),t&&n(B),t&&n(I),t&&n(g),t&&n(k),t&&n(D),t&&n(F),t&&n(L),t&&n(T),t&&n(W),t&&n(q),t&&n(K)}}}function tt(_){let a,f,o,b,c,s,w,p,d;return o=new N({props:{weight:"bold",size:"4xl",$$slots:{default:[Y]},$$scope:{ctx:_}}}),s=new N({props:{weight:"light",size:"2xl",$$slots:{default:[Z]},$$scope:{ctx:_}}}),{c(){a=u("div"),f=x(),R(o.$$.fragment),b=u("br"),c=y(`
+
+		// Adding About Us copy here, sorry if I messed up the html formatting what nots, just getting
+		the text in whatever way I can.
+
+		`),R(s.$$.fragment),w=x(),p=u("div"),this.h()},l(e){a=m(e,"DIV",{class:!0}),j(a).forEach(n),f=h(e),S(o.$$.fragment,e),b=m(e,"BR",{}),c=v(e,`
+
+		// Adding About Us copy here, sorry if I messed up the html formatting what nots, just getting
+		the text in whatever way I can.
+
+		`),S(s.$$.fragment,e),w=h(e),p=m(e,"DIV",{class:!0}),j(p).forEach(n),this.h()},h(){U(a,"class","h-8"),U(p,"class","min-h-screen")},m(e,i){r(e,a,i),r(e,f,i),A(o,e,i),r(e,b,i),r(e,c,i),A(s,e,i),r(e,w,i),r(e,p,i),d=!0},p(e,i){const $={};i&1&&($.$$scope={dirty:i,ctx:e}),o.$set($);const B={};i&1&&(B.$$scope={dirty:i,ctx:e}),s.$set(B)},i(e){d||(E(o.$$.fragment,e),E(s.$$.fragment,e),d=!0)},o(e){C(o.$$.fragment,e),C(s.$$.fragment,e),d=!1},d(e){e&&n(a),e&&n(f),z(o,e),e&&n(b),e&&n(c),z(s,e),e&&n(w),e&&n(p)}}}function et(_){let a,f,o,b,c,s,w,p,d;return b=new J({}),s=new X({props:{$$slots:{default:[tt]},$$scope:{ctx:_}}}),p=new Q({}),{c(){a=u("meta"),f=x(),o=u("section"),R(b.$$.fragment),c=x(),R(s.$$.fragment),w=x(),R(p.$$.fragment),this.h()},l(e){const i=P('[data-svelte="svelte-13qttjz"]',document.head);a=m(i,"META",{name:!0,content:!0}),i.forEach(n),f=h(e),o=m(e,"SECTION",{});var $=j(o);S(b.$$.fragment,$),c=h($),S(s.$$.fragment,$),w=h($),S(p.$$.fragment,$),$.forEach(n),this.h()},h(){document.title="About",U(a,"name","description"),U(a,"content","About")},m(e,i){V(document.head,a),r(e,f,i),r(e,o,i),A(b,o,null),V(o,c),A(s,o,null),V(o,w),A(p,o,null),d=!0},p(e,[i]){const $={};i&1&&($.$$scope={dirty:i,ctx:e}),s.$set($)},i(e){d||(E(b.$$.fragment,e),E(s.$$.fragment,e),E(p.$$.fragment,e),d=!0)},o(e){C(b.$$.fragment,e),C(s.$$.fragment,e),C(p.$$.fragment,e),d=!1},d(e){n(a),e&&n(f),e&&n(o),z(b),z(s),z(p)}}}class lt extends O{constructor(a){super(),M(this,a,null,et,H,{})}}export{lt as default};
